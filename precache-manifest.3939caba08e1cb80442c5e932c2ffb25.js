a7f301a2e82c221d4d23de1708b5b6465dee647c
divyank1997 <divyanksrivastava1997@gmail.com>
2019-06-29 10:32:47 +0530

    New repo

---
+++ precache-manifest.3939caba08e1cb80442c5e932c2ffb25.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "c1b887fbc08b7753f9bb",
+    "url": "/static/css/main.bfbd5979.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "046c555d2b558b7710b2",
+    "url": "/static/js/2.5c546e68.chunk.js"
+  },
+  {
+    "revision": "c1b887fbc08b7753f9bb",
+    "url": "/static/js/main.4d314d9c.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "046c555d2b558b7710b2",
+    "url": "/static/css/2.ca6b3176.chunk.css"
+  },
+  {
+    "revision": "2d141ce9ff9f08830475aee2a9f7a580",
+    "url": "/index.html"
+  }
+];
